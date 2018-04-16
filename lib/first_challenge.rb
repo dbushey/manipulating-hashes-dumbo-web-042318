55ce06e9e69827ca6ac310b855ef462f4be40b3e
Daiane Bushey <daianebushey@gmail.com>
2018-04-16 06:37:22 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ def first_challenge
 
       data.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
-          attribute.delete_if("strawberry")
+          value.delete_if("strawberry")
       end
     end
   end
